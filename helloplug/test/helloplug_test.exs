d845e99e3038fa99dc5e2e8bb453199fa78f6984
Dean Chouinard <deanchouinard@gmail.com>
2016-12-09 22:41:39 -0500

    tests and added README

--- helloplug/test/helloplug_test.exs
+++ helloplug/test/helloplug_test.exs
@@ -44,6 +44,25 @@ defmodule HelloplugTest do
 
   end
 
+  test "WebsiteRouter user page" do
+    conn = conn(:get, "/users/6")
+    conn = WebsiteRouter.call(conn, [])
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "You requested user number 6"
+
+  end
+  
+  test "WebsiteRouter missing page" do
+    conn = conn(:get, "/missing")
+    conn = WebsiteRouter.call(conn, [])
+
+    assert conn.state == :sent
+    assert conn.status == 404
+    assert conn.resp_body == "Could not find page."
+
+  end
   test "the truth" do
     assert 1 + 1 == 2
   end
